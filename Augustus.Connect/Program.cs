365ac1a9ba286507d14fc33cd7e0fae12c4bd236
gothandy <gothandy@hotmail.com>
2016-02-25 16:47:57 +0000

    Cut and paste code.

--- Augustus.Connect/Program.cs
+++ Augustus.Connect/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 
 namespace Augustus.Connect
 {
@@ -10,6 +7,23 @@ namespace Augustus.Connect
     {
         static void Main(string[] args)
         {
+            //https://msdn.microsoft.com/en-US/library/gg327838.aspx
+
+            // TODO Substitute your correct CRM root service address, 
+            string resource = "https://mydomain.crm.dynamics.com";
+
+            // TODO Substitute your app registration values that can be obtained after you
+            // register the app in Active Directory on the Microsoft Azure portal.
+            string clientId = "e5cf0024-a66a-4f16-85ce-99ba97a24bb2";
+            string redirectUrl = "http://localhost/SdkSample";
+
+
+            // Authenticate the registered application with Azure Active Directory.
+            AuthenticationContext authContext =
+                new AuthenticationContext("https://login.windows.net/common", false);
+            AuthenticationResult result = authContext.AcquireToken(resource, clientId, new
+                                                                   Uri(redirectUrl));
+
         }
     }
 }
