1d80772de6e5f1beb76532ac44c19c7e140172f7
gothandy <gothandy@hotmail.com>
2016-02-26 16:17:05 +0000

    Added XRM tooling

--- Augustus.Connect/Program.cs
+++ Augustus.Connect/Program.cs
@@ -1,21 +1,45 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Xrm.Tooling.Connector;
 using System;
 
 namespace Augustus.Connect
 {
     class Program
     {
+        const string resource = "https://trueclarity4.crm4.dynamics.com";
+        const string clientId = "adb8518c-0996-4cdb-9f93-a2a1c13c9456";
+        const string redirectUrl = "http://augustus.trueclarity.co.uk/crm";
+
         static void Main(string[] args)
+        {
+            xrmConnectionString();
+        }
+
+        private static void xrmConnectionString()
+        {
+            string connectionString =
+                string.Format("AuthType=OAuth;Url={0};AppId={1};RedirectUri={2};TokenCacheStorePath ={3};LoginPrompt=Auto",
+                resource,
+                clientId,
+                redirectUrl,
+                "C:\\TC\\Augustus");
+
+            //Use the connection string named "MyCRMServer"
+            //from the configuration file
+            CrmServiceClient crmSvc = new CrmServiceClient(connectionString);
+
+        }
+
+        private static void oauthFirst()
         {
             //https://msdn.microsoft.com/en-US/library/gg327838.aspx
 
             // TODO Substitute your correct CRM root service address, 
-            string resource = "https://mydomain.crm.dynamics.com";
+            
 
             // TODO Substitute your app registration values that can be obtained after you
             // register the app in Active Directory on the Microsoft Azure portal.
-            string clientId = "e5cf0024-a66a-4f16-85ce-99ba97a24bb2";
-            string redirectUrl = "http://localhost/SdkSample";
+
 
 
             // Authenticate the registered application with Azure Active Directory.
@@ -23,6 +47,7 @@ namespace Augustus.Connect
                 new AuthenticationContext("https://login.windows.net/common", false);
             AuthenticationResult result = authContext.AcquireToken(resource, clientId, new
                                                                    Uri(redirectUrl));
+
 
         }
     }
