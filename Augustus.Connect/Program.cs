6428562403997f88d084c8234baf76c44fbef2c7
gothandy <gothandy@hotmail.com>
2016-02-27 13:31:51 +0000

    Got the basics of talking to CRM in place.

--- Augustus.Connect/Program.cs
+++ Augustus.Connect/Program.cs
@@ -1,54 +1,101 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
 using System;
+using System.Configuration;
 
 namespace Augustus.Connect
 {
     class Program
     {
-        const string resource = "https://trueclarity4.crm4.dynamics.com";
-        const string clientId = "adb8518c-0996-4cdb-9f93-a2a1c13c9456";
-        const string redirectUrl = "http://augustus.trueclarity.co.uk/crm";
 
         static void Main(string[] args)
         {
             xrmConnectionString();
+
+            Console.ReadKey();
         }
 
         private static void xrmConnectionString()
         {
-            string connectionString =
-                string.Format("AuthType=OAuth;Url={0};AppId={1};RedirectUri={2};TokenCacheStorePath ={3};LoginPrompt=Auto",
-                resource,
-                clientId,
-                redirectUrl,
-                "C:\\TC\\Augustus");
+            string connectionString = ConfigurationManager.AppSettings["AugustusCRM"];
 
-            //Use the connection string named "MyCRMServer"
-            //from the configuration file
             CrmServiceClient crmSvc = new CrmServiceClient(connectionString);
 
+            // Verify that you are connected.
+            if (crmSvc != null && crmSvc.IsReady)
+            {
+                IOrganizationService _orgService = GetAndCastProxyClientFromWebOrService(crmSvc);
+
+                WriteInvoices(_orgService);
+            }
+            else
+            {
+                DisplayLastError(crmSvc);
+            }
         }
 
-        private static void oauthFirst()
+        private static IOrganizationService GetAndCastProxyClientFromWebOrService(CrmServiceClient crmSvc)
         {
-            //https://msdn.microsoft.com/en-US/library/gg327838.aspx
 
-            // TODO Substitute your correct CRM root service address, 
-            
+            // Cast the proxy client to the IOrganizationService interface.
+            return (IOrganizationService)crmSvc.OrganizationWebProxyClient != null ?
+                (IOrganizationService)crmSvc.OrganizationWebProxyClient :
+                (IOrganizationService)crmSvc.OrganizationServiceProxy;
+        }
 
-            // TODO Substitute your app registration values that can be obtained after you
-            // register the app in Active Directory on the Microsoft Azure portal.
+        private static void DisplayLastError(CrmServiceClient crmSvc)
+        {
+            Console.WriteLine("Error occurred: {0}", crmSvc.LastCrmError);
 
+            // Display the last exception message if any.
+            Console.WriteLine(crmSvc.LastCrmException.Message);
+            Console.WriteLine(crmSvc.LastCrmException.Source);
+            Console.WriteLine(crmSvc.LastCrmException.StackTrace);
+        }
 
+        private static void WriteInvoices(IOrganizationService _orgService)
+        {
+            QueryExpression query = new QueryExpression
+            {
+                EntityName = new_invoice.EntityLogicalName,
+                ColumnSet = new ColumnSet(
+                        "new_invoicedate",
+                        "new_directclient",
+                        "new_margin",
+                        "new_invoiceid",
+                        "statuscode",
+                        "new_wip_previous",
+                        "new_wip_current",
+                        "new_name")
+            };
 
-            // Authenticate the registered application with Azure Active Directory.
-            AuthenticationContext authContext =
-                new AuthenticationContext("https://login.windows.net/common", false);
-            AuthenticationResult result = authContext.AcquireToken(resource, clientId, new
-                                                                   Uri(redirectUrl));
+            foreach (new_invoice invoice in _orgService.RetrieveMultiple(query).Entities)
+            {
+                object[] values = {
+                    invoice.new_invoiceId,
+                    invoice.new_InvoiceDate.GetValueOrDefault().ToShortDateString(),
+                    invoice.new_DirectClient == null ?  null : invoice.new_DirectClient.Name,
+                    getMoneyValue(invoice.new_Margin),
+                    getMoneyValue(invoice.new_wip_previous),
+                    getMoneyValue(invoice.new_WIP_Current),
+                    invoice.statecode,
+                    invoice.new_name};
 
+                Console.WriteLine(string.Join(", ", values));
+            };
+        }
 
+        private static decimal? getMoneyValue(Money money)
+        {
+            if (money == null)
+            {
+                return null;
+            }
+            else
+            {
+                return money.Value;
+            }
         }
     }
 }
