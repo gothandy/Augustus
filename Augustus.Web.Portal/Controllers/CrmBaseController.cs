1e6373ad23ba2a9598165c782c703e688c296abd
gothandy <gothandy@hotmail.com>
2016-03-22 09:47:13 +0000

    Try ignoring exception.

--- Augustus.Web.Portal/Controllers/CrmBaseController.cs
+++ Augustus.Web.Portal/Controllers/CrmBaseController.cs
@@ -48,7 +48,8 @@ namespace Augustus.Web.Portal.Controllers
 
             if (capturedException != null)
             {
-                throw new Exception("Caught it. Now what?", capturedException.SourceException);
+
+                //throw new Exception("Caught it. Now what?", capturedException.SourceException);
             }
 
             return result;    
