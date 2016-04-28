3bad89dc9a10268fa726bb7829dcac94508b65c3
gothandy <gothandy@hotmail.com>
2016-04-28 08:19:09 +0100

    Export to XML in Azure Blob Storage for Excel Query.

---
+++ Augustus.Console.Export/Export.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Augustus.Console.Export
+{
+    [XmlRoot("Export")]
+    public class Export : List<Item>
+    {
+
+    }
+
+    [XmlRoot("Item")]
+    public class Item
+    {
+        public Item()
+        {
+        }
+
+        [XmlElement("AccountName")]
+        public string AccountName { get; set; }
+
+        [XmlElement("InvoiceName")]
+        public string InvoiceName { get; set; }
+
+        [XmlElement("InvoiceDate")]
+        public DateTime InvoiceDate{ get; set; }
+
+        [XmlElement("InvoiceMargin")]
+        public Decimal InvoiceMargin { get; set; }
+
+        [XmlElement("WorkDoneDate")]
+        public DateTime WorkDoneDate { get; set; }
+
+        [XmlElement("WorkDoneMargin")]
+        public Decimal WorkDoneMargin { get; set; }
+    }
+}
---
+++ Augustus.Console.Export/Program.cs
@@ -0,0 +1,63 @@
+﻿using Augustus.CRM;
+using Augustus.CRM.Queries;
+using Augustus.Domain.Objects;
+using Augustus.Web.Config;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Linq;
+using System;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Augustus.Console.Export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new CrmServiceConnectionString(AppSettings.CrmConnectionString);
+            var context = service.GetContext();
+            var query = new ReportQuery(context);
+            var data = query.GetExportData();
+
+            var export = new Export();
+
+            foreach(var w in data)
+            {
+                export.Add(new Item
+                {
+                    AccountName = w.Account.Name,
+                    InvoiceName = w.Invoice.Name,
+                    InvoiceDate = MonthStart(w.Invoice.InvoiceDate),
+                    InvoiceMargin = w.Invoice.Margin.GetValueOrDefault(),
+                    WorkDoneDate = MonthStart(w.WorkDoneItem.WorkDoneDate),
+                    WorkDoneMargin = w.WorkDoneItem.Margin.GetValueOrDefault()
+                });
+            }
+
+            var azureConnectionString = "DefaultEndpointsProtocol=https;AccountName=augustusexport;AccountKey=MRR/7VNJTLCgpoVicRDICk7USjCcuczZqXcBC9WYsNfLGE8Mfk2Utt2tTSfCLfvNSLgyuXjA3hvg9q4pfIP5RQ==";
+            var azureStorageAccount = CloudStorageAccount.Parse(azureConnectionString);
+            var azureBlobClient = azureStorageAccount.CreateCloudBlobClient();
+            var azureBlobContainer = azureBlobClient.GetContainerReference("workdoneexport");
+
+            var blob = azureBlobContainer.GetBlockBlobReference("export.xml");
+            var xml = Serialize<Export>(export);
+            blob.UploadText(xml);
+        }
+
+       
+
+        private static string Serialize<T>(T dataToSerialize)
+        {
+            var stringwriter = new System.IO.StringWriter();
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(stringwriter, dataToSerialize);
+            return stringwriter.ToString();
+        }
+
+        private static DateTime MonthStart(DateTime? date)
+        {
+            return new DateTime(date.Value.Year, date.Value.Month, 1);
+        }
+    }
+}
